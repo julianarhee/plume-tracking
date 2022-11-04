a45d852324d1ba37328aedd012d3bc4a79f2f0f5
rutalab <juliana.rhee@gmail.com>
2022-11-04 15:39:54 -0400

    add shebang and comments

--- sample-plot-trajectory.py
+++ sample-plot-trajectory.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   sample-plot-trajectory.py
+@Time    :   2022/10/28 17:32:55
+@Author  :   julianarhee 
+@Contact :   juliana.rhee@gmail.com
+ 
+'''
 import os
 import glob
 import sys
@@ -23,45 +29,46 @@ import behavior as butil
 
 util.set_sns_style(style='dark') # plotting settings i like for Nbs
 
-def plot_trajectory(experiment, fly, rootdir='/Users/julianarhee/Library/CloudStorage/GoogleDrive-edge.tracking.ru@gmail.com/My Drive/Edge_Tracking/Data'):
+def plot_trajectory(experiment, fly, user_input=False, 
+        odor_width=50, grid_sep=2000,
+        rootdir='/Users/julianarhee/Library/CloudStorage/GoogleDrive-edge.tracking.ru@gmail.com/My Drive/Edge_Tracking/Data'):
 
     src_dir = os.path.join(rootdir, experiment)
-    # Create a label/ID for figures so you always know where the plotted data came from
+    # Create an ID for figures (path to experiment dir)
     fig_id = '{}: {}'.format(src_dir.split('/My Drive')[1], fly)
 
+    # Get a list of all the data files
+    if not user_input:
+        # try to find datafile(s) automatically by fly name (e.g.,'fly3')
+        try:
+            log_files = sorted([k for k in glob.glob(os.path.join(src_dir, '*{}*.log'.format(fly)))\
+                        if 'lossed tracking' not in k], key=util.natsort)
+            assert len(log_files)==1, "Unique datafile not found. Manually select file." 
+            fpath = log_files[0]
+        except Exception as e:
+            user_input=True
+            log_files = sorted([k for k in glob.glob(os.path.join(src_dir, \
+                            '*.log')) if 'lossed tracking' not in k], \
+                            key=util.natsort) 
 
-    # get a list of all the data files
-    try:
-        log_files = sorted([k for k in glob.glob(os.path.join(src_dir, '*{}*.log'.format(fly)))\
-                    if 'lossed tracking' not in k], key=util.natsort)
-        assert len(log_files)>1, "Fly not found, trying with diff. case"
-    except Exception as e:
-        log_files = sorted([k for k in glob.glob(os.path.join(src_dir, '*.log'))\
-                    if 'lossed tracking' not in k], key=util.natsort)
-        print("Unable to find your fly file...")
-        for fpath in log_files:
-            print('...{}'.format(os.path.split(fpath)[-1]))
- 
-    print("Found {} tracking files.".format(len(log_files)))
-    for i, fn in enumerate(log_files):
-        print("{}: {}".format(i, os.path.split(fn)[-1]))
-
-    file_ix = int(input('Select IX of file to run: ')) # 12 # select a file
-    fpath = log_files[file_ix]
-    print("Selected: {}".format(fpath))
+    # List all files and have user select index of file to process
+    if user_input:
+        print("Found {} tracking files.".format(len(log_files)))
+        for i, fn in enumerate(log_files):
+            print("{}: {}".format(i, os.path.split(fn)[-1]))
+        file_ix = int(input('Select IX of file to run: ')) # 12 # select a file
+        fpath = log_files[file_ix]
+        print("Selected: {}".format(fpath))
 
     # try to parse experiment details from the filename
-    exp, fid, cond = butil.parse_info_from_file(fpath)
+    exp, datestr, fid, cond = butil.parse_info_from_file(fpath)
     # load and process the csv data  
     df0 = butil.load_dataframe(fpath, mfc_id=None, verbose=False, cond=None)
     print('Experiment: {}{}Fly ID: {}{}Condition: {}'.format(exp, '\n', fid, '\n', cond))
     fly_id = df0['fly_id'].unique()[0]
 
     # get experimentally determined odor boundaries:
-    odor_width=50
-    grid_sep=2000 # only matters for 2p
-    ogrid = butil.get_odor_grid(df0, 
-                                odor_width=odor_width, grid_sep=grid_sep,
+    ogrid = butil.get_odor_grid(df0, odor_width=odor_width, grid_sep=grid_sep,
                                 use_crossings=True, verbose=False)
     (odor_xmin, odor_xmax), = ogrid.values()
 
