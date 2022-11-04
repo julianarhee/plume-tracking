fa73b95584037c4e7b7a1c22eb92bf1fe7a3030e
rutalab <juliana.rhee@gmail.com>
2022-11-04 16:50:19 -0400

    add opt to parse info from filename

--- sample-plot-trajectory.py
+++ sample-plot-trajectory.py
@@ -29,21 +29,18 @@ import behavior as butil
 
 util.set_sns_style(style='dark') # plotting settings i like for Nbs
 
-def plot_trajectory(experiment, datestr, user_input=False, 
-        odor_width=50, grid_sep=2000,
+def plot_trajectory(experiment, datestr, parse_info=False, user_input=False, 
+        odor_width=10, grid_sep=200,
         rootdir='/Users/julianarhee/Library/CloudStorage/GoogleDrive-edge.tracking.ru@gmail.com/My Drive/Edge_Tracking/Data'):
 
     src_dir = os.path.join(rootdir, experiment)
     # Create an ID for figures (path to experiment dir)
     fig_id = '{}: {}'.format(src_dir.split(rootdir)[1], datestr)
 
-    # Get a list of all the data files
-    log_files = sorted([k for k in glob.glob(os.path.join(src_dir, '*{}*.log'.format(datestr)))], key=util.natsort)
-    assert len(log_files)==1, "No unique file found..."
-    fpath = log_files[0]
 
     # List all files and have user select index of file to process
     if user_input:
+        # Get a list of all the data files
         log_files = sorted([k for k in glob.glob(os.path.join(src_dir, \
                             '*.log')) if 'lossed tracking' not in k], \
                             key=util.natsort) 
@@ -53,13 +50,21 @@ def plot_trajectory(experiment, datestr, user_input=False,
         file_ix = int(input('Select IX of file to run: ')) # 12 # select a file
         fpath = log_files[file_ix]
         print("Selected: {}".format(fpath))
+    else:
+        # Find datafile with datestr in it (there should only be one)
+        log_files = sorted([k for k in glob.glob(os.path.join(src_dir, '*{}*.log'.format(datestr)))], key=util.natsort)
+        assert len(log_files)==1, "No unique file found..."
+        fpath = log_files[0]
 
-    # try to parse experiment details from the filename
-    exp, fid, cond = butil.parse_info_from_file(fpath)
     # load and process the csv data  
-    df0 = butil.load_dataframe(fpath, mfc_id=None, verbose=False, cond=None)
-    print('Experiment: {}{}Fly ID: {}{}Condition: {}'.format(exp, '\n', fid, '\n', cond))
-    fly_id = df0['fly_id'].unique()[0]
+    df0 = butil.load_dataframe(fpath, mfc_id=None, verbose=False, cond=None, 
+                parse_info=False)
+    fly_id=None
+    if parse_info:
+        # try to parse experiment details from the filename
+        exp, fid, cond = butil.parse_info_from_file(fpath)
+        print('Experiment: {}{}Fly ID: {}{}Condition: {}'.format(exp, '\n', fid, '\n', cond))
+        fly_id = df0['fly_id'].unique()[0]
 
     # get experimentally determined odor boundaries:
     ogrid = butil.get_odor_grid(df0, odor_width=odor_width, grid_sep=grid_sep,
@@ -79,15 +84,16 @@ def plot_trajectory(experiment, datestr, user_input=False,
                     hue=hue_varname, s=0.5, edgecolor='none', palette=palette)
     butil.plot_odor_corridor(ax, odor_xmin=odor_xmin, odor_xmax=odor_xmax)
     ax.legend(bbox_to_anchor=(1,1), loc='upper left', title=hue_varname)
-    ax.set_title(fly_id)
+    if fly_id is not None:
+        ax.set_title(fly_id)
+    else:
+        ax.set_title(datestr)
     pl.subplots_adjust(left=0.2, right=0.8)
     # Center corridor
     xmax = np.ceil(df0['ft_posx'].abs().max())
     ax.set_xlim([-xmax-10, xmax+10])
     # label figure and save
     util.label_figure(fig, fig_id)
-    figname = 'trajectory_{}'.format(fly_id)
-
     pl.show()
 
     #pl.savefig(os.path.join(save_dir, '{}.png'.format(figname))) #, dpi=dpi)
