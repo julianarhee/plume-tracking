bae7a981a6302fcc01f1ae169807b24b8ff88fae
Juliana <juliana.rhee@gmail.com>
2022-11-27 14:49:26 -0500

    update quickstart to include path to parent dir

--- quickstart.py
+++ quickstart.py
@@ -16,7 +16,7 @@ SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
 SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 
 
-def main():
+def main(parent_dir = '/Users/julianarhee/Repositories/edge-tracking'):
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
@@ -24,18 +24,18 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists(os.path.join(parent_dir, 'token.json')):
+        creds = Credentials.from_authorized_user_file(os.path.join(parent_dir, 'token.json'), SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                os.path.join(parent_dir, 'credentials.json'), SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open(os.path.join(parent_dir, 'token.json'), 'w') as token:
             token.write(creds.to_json())
 
     try:
