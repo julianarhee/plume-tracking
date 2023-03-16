6b25b5aa28fbdf4e34344df221729ed7426a40c1
Juliana Rhee <juliana.rhee@gmail.com>
2023-03-16 19:41:19 -0400

    moving zero_start_position to plotting since only for visualization

--- behavior.py
+++ behavior.py
@@ -2788,6 +2788,7 @@ def get_bout_metrics(etdf1):
     for c in boutdf.columns.tolist():
         if not pd.api.types.is_numeric_dtype(boutdf[c]):
             check_cols.append(c)
+    assert len(check_cols)==0, "Bad dtypes"
 
     return boutdf
 
@@ -2843,16 +2844,6 @@ def summarize_stops_and_turns(df_, meanangs_, last_,  strip_width=10, strip_sep=
     pl.subplots_adjust(left=0.1, wspace=0.5, top=0.8, right=0.85, bottom=0.2)
     return fig
 
-
-def zero_start_position(b_):
-    b_['ft_posx_start0'] = b_['ft_posx'] - b_['ft_posx'].iloc[0]
-    b_['ft_posy_start0'] = b_['ft_posy'] - b_['ft_posy'].iloc[0]
-    return b_
-
-def normalize_position(b_):
-    b_['ft_posx_norm'] = util.convert_range(b_['ft_posx_start0'], newmin=0, newmax=1)
-    b_['ft_posy_norm'] = util.convert_range(b_['ft_posy_start0'], newmin=0, newmax=1)
-    return b_
 
 
 # ----------------------------------------------------------------------
