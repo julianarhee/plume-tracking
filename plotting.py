afa354bbaf2d8e3027c9bc4d5c2fa6938d077ea2
Juliana Rhee <juliana.rhee@gmail.com>
2023-03-16 14:53:54 -0400

    add nice traj plotting funcs

--- plotting.py
+++ plotting.py
@@ -19,28 +19,50 @@ import seaborn as sns
 import utils as util
 import behavior as butil
 
+## generic
+# ticks
+def set_outward_spines(ax):
+    ax.tick_params(which='both', axis='both', length=2, width=0.5, color='w',
+               direction='out', left=True)
+
+def remove_spines(ax, axes=['right', 'top']):
+    for pos in axes:
+       ax.spines[pos].set_visible(False)
+
+
 ##
 
+def zero_trajectory(df_):
+
+    plotdf = df_.copy()
+    offset_x = plotdf[plotdf['instrip']].iloc[0]['ft_posx']
+    offset_y = plotdf[plotdf['instrip']].iloc[0]['ft_posy']
+    plotdf['ft_posx'] = plotdf['ft_posx'].values - offset_x
+    plotdf['ft_posy'] = plotdf['ft_posy'].values - offset_y
+    
+    odor_ix = plotdf[plotdf['instrip']].iloc[0].name
+
+    return plotdf.loc[odor_ix:]
+
+
+
 def plot_zeroed_trajectory(df_, ax=None, traj_lw=1.5, odor_lw=1.0,
-                        strip_width=50, strip_sep=500, 
+                        strip_width=50, strip_sep=1000, main_col='w',
                         bool_colors=['r'], bool_vars=['instrip']):
     if ax is None:
         fig, ax= pl.subplots()
     odor_ix = df_[df_['instrip']].iloc[0].name
     #plotdf = df_.loc[odor_ix:]
     # odor_ix = params[fn]['odor_ix']
-    plotdf = df_.copy()
-    offset_x = plotdf[plotdf['instrip']].iloc[0]['ft_posx']
-    offset_y = plotdf[plotdf['instrip']].iloc[0]['ft_posy']
-    plotdf['ft_posx'] = plotdf['ft_posx'].values - offset_x
-    plotdf['ft_posy'] = plotdf['ft_posy'].values - offset_y
+    plotdf = zero_trajectory(df_)
+    odor_ix = plotdf[plotdf['instrip']].iloc[0].name
     odor_bounds = butil.find_strip_borders(plotdf, entry_ix=odor_ix,
                                         strip_width=strip_width,
                                         strip_sep=strip_sep)
     # plot
     plotdf = plotdf.loc[odor_ix:].copy()
     
-    ax.plot(plotdf['ft_posx'], plotdf['ft_posy'], lw=traj_lw, c='w')
+    ax.plot(plotdf['ft_posx'], plotdf['ft_posy'], lw=traj_lw, c=main_col)
     for ob in odor_bounds:
         butil.plot_odor_corridor(ax, odor_xmin=ob[0], 
                              odor_xmax=ob[1], odor_linewidth=odor_lw)
@@ -48,19 +70,23 @@ def plot_zeroed_trajectory(df_, ax=None, traj_lw=1.5, odor_lw=1.0,
     for col, boolvar in zip(bool_colors, bool_vars):
         for bnum, b_ in plotdf[plotdf[boolvar]].groupby('boutnum'):
             ax.plot(b_['ft_posx'], b_['ft_posy'], lw=traj_lw, c=col)
+
     return ax
 
 
 def plot_paired_inout_metrics(df_, nr=2, nc=3,
-                varnames=['duration', 'path_length',
+                xvarname='instrip', order=[False, True], xticklabels=['outstrip', 'instrip'],
+                yvarnames=['duration', 'path_length',
                 'crosswind_speed', 'upwind_speed', 
                 'crosswind_dist_range', 'upwind_dist_range']):
 
 
     fig, axn = pl.subplots(nr, nc, figsize=(10,5)) #len(varnames))
-    for ax, varn in zip(axn.flat, varnames):
-        plot_paired_in_vs_out(varn, df_, ax=ax)
-        a = df_[df_['instrip']][['filename', varn]]
+    for ax, varn in zip(axn.flat, yvarnames):
+        plot_paired_in_vs_out(varn, df_, ax=ax, 
+                    xvarname=xvarname, order=order, xticklabels=xticklabels)
+        #a = df_[df_['instrip']][['filename', varn]]
+        a = df_['filename'].unique()
         dof = len(a)-1
         fig.text(0.75, 0.06, 'Wilcoxon signed-rank, n={} trajectories'.format(len(a)),
                 fontsize=10)
@@ -68,16 +94,24 @@ def plot_paired_inout_metrics(df_, nr=2, nc=3,
     
     return fig
 
-def plot_paired_in_vs_out(varn, df_, ax=None):
+def plot_paired_in_vs_out(varn, df_, ax=None, 
+                xvarname='instrip', order=[False, True],
+                xticklabels=['outstrip', 'instrip']):
     if ax is None:
         fig, ax = pl.subplots()
     # plot
-    df_['instrip'] = df_['instrip'].astype(int)
-    sns.stripplot(data=df_, x='instrip', y=varn, ax=ax, c='w', s=3, jitter=False)
+    is_bool = False
+    if df_[xvarname].dtype == 'bool': 
+        df_[xvarname] = df_[xvarname].astype(int)
+        is_bool = True
+        order = order.astype(int)
+    sns.stripplot(data=df_, x=xvarname, y=varn, ax=ax, order=order,
+                    c='w', s=3, jitter=False)
     # plot paired lines
+    v1, v2 = order
     for f, fd in df_.groupby('filename'):
         ax.plot([0, 1],
-            [fd[fd['instrip']==0][varn], fd[fd['instrip']==1][varn]],
+            [fd[fd[xvarname]==v1][varn], fd[fd[xvarname]==v2][varn]],
                 'w', lw=0.5)
     # adjust ticks
     ax.tick_params(which='both', axis='both', length=2, width=0.5, color='w',
@@ -87,10 +121,10 @@ def plot_paired_in_vs_out(varn, df_, ax=None):
     # adjust labels
     ax.set_xlabel('')
     ax.set_xlim([-0.5, 1.5])
-    ax.set_xticklabels(['outstrip', 'instrip'])
+    ax.set_xticklabels(xticklabels) #['outstrip', 'instrip'])
     # stats
-    a = df_[df_['instrip']==0][varn].values
-    b = df_[df_['instrip']==1][varn].values
+    a = df_[df_[xvarname]==v1][varn].values
+    b = df_[df_[xvarname]==v2][varn].values
     pdf = pd.DataFrame({'a': a, 'b': b})
     T, pv = spstats.wilcoxon(pdf["a"], pdf["b"], nan_policy='omit')
     if pv>=0.05:
@@ -99,14 +133,127 @@ def plot_paired_in_vs_out(varn, df_, ax=None):
         star = '**' if pv<0.01 else '*'
     ax.set_title(star, fontsize=8)
 
-    df_['instrip'] = df_['instrip'].astype(bool)
+    if is_bool: 
+        df_[xvarname] = df_[xvarname].astype(bool)
 
     return ax
 
 
+def flip_data_for_abutting_hists(boutdf_filt,
+        hue='instrip', hue_values=[True, False],
+        vars_to_abs = ['speed', 'upwind_speed', 'crosswind_speed'],
+        vars_to_flip = ['duration', 'abs_speed', 'path_length', 
+                        'path_length_x', 'path_length_y', 'crosswind_dist_range', 
+                        'abs_upwind_speed', 'upwind_dist_range', 'abs_crosswind_speed']):
+    '''
+    Flip inside bout metrics so that in and out histograms and plots can be 
+    visualized abutting each other. Need to do this for plot_sorted_distn_with_hist()
+    
+    Returns boutdf with `_flipped` appended to flipped variables, and `abs_` prefixed to absolute valued variables. 
+    '''
+    v1, v2 = hue_values
+    for varn in vars_to_abs:
+        boutdf_filt['abs_{}'.format(varn)] = boutdf_filt[varn].abs()
+
+    for varn in vars_to_flip:
+        new_varn = '{}_flipped'.format(varn)
+        boutdf_filt[new_varn] = None
+        boutdf_filt.loc[boutdf_filt[hue]==v1, new_varn]  = -1*boutdf_filt.loc[boutdf_filt[hue]==v1, varn].values
+        boutdf_filt.loc[boutdf_filt[hue]==v2, new_varn]  = 1*boutdf_filt.loc[boutdf_filt[hue]==v2, varn].values
+        boutdf_filt[new_varn] = boutdf_filt[new_varn].astype(float)
+        
+    return boutdf_filt
+
 def plot_sorted_distn_with_hist(varn, boutdf_filt, estimator='median',
                              plot_bars=False, errorbar=('ci', 95),
+                            hue='instrip', hue_values=[True, False], hue_colors=['r', 'w']):
+    '''
+    Plot each individual fly's bouts as dot plot, abutting inside and outside.
+    Below dot plot, show abutting histograms aligned.
+
+    '''
+    xlabels = {
+        'crosswind_dist_range': 'crosswind range from edge (mm)',
+        'duration':'duration',
+        'path_length': 'path length (mm)',
+        'path_length_x': 'x-path length (mm)',
+        'path_length_y': 'y-path length(mm)',
+        'abs_speed': 'abs. speed (mm/s)',
+        'abs_upwind_speed': 'abs. upwind speed (mm/s)',
+        'abs_crosswind_speed': 'abs. crosswind speed (mm/s)',
+        'upwind_dist_range': 'upwind range (mm)',
+        'crosswind_dist_range': 'crosswind range (mm)'
+    }
+    plotvar = '{}_flipped'.format(varn) 
+
+    if estimator == 'median':
+        mean_boutdf_filt2 = boutdf_filt.groupby(['filename', hue]).median().reset_index()
+    elif estimator == 'mean':
+        mean_boutdf_filt2 = boutdf_filt.groupby(['filename', hue]).mean().reset_index()
+    else:
+        mean_boutdf_filt2 = boutdf_filt.groupby(['filename', hue]).max().reset_index()
+    # sort by OUT
+    v1, v2 = hue_values
+    sorted_by_xwind_out = mean_boutdf_filt2[mean_boutdf_filt2[hue]==v2]\
+                            .sort_values(by=plotvar)['filename'].values
+
+    # ---- plot ------
+    xlabel = xlabels[varn]
+    palette = dict((k, v) for k, v in zip(hue_values, hue_colors))
+    nbins = 20 if varn=='path length' else 200 #if 
+    fig = pl.figure(figsize=(6,8)) # sharex=True)
+    gs = mpl.gridspec.GridSpec(3,1, figure=fig )
+    # create sub plots as grid
+    ax1 = fig.add_subplot(gs[0:2])
+    if plot_bars:
+        assert estimator in ['median', 'mean'], "wrong estimator: {}".format(estimator)
+        sns.barplot(data=boutdf_filt, x=plotvar, ax=ax1, 
+                y='filename', order=sorted_by_xwind_out,
+                hue=hue, palette=palette, edgecolor='none', dodge=False,
+               errcolor=[0.5]*3, errorbar=('ci', 95), estimator=estimator,
+                saturation=1, width=0.8)
+    else:
+        sns.stripplot(data=boutdf_filt, x=plotvar, ax=ax1, 
+                    y='filename', order=sorted_by_xwind_out,
+                    hue=hue, palette=palette, edgecolor='none', dodge=False,
+                    alpha=0.5)
+    ax1.set_yticklabels([i if i%2==0 else '' for i, f in enumerate(sorted_by_xwind_out)])
+    ax1.set_ylabel('traj. id')
+    ax1.set_xlabel('')
+    sns.move_legend(ax1, bbox_to_anchor=(1,1), loc='lower right', frameon=False)
+
+    # histogram underneath
+    vmin, vmax = boutdf_filt[plotvar].min(), boutdf_filt[plotvar].max()
+    ax2 = fig.add_subplot(gs[2], sharex=ax1)
+    for col, cond in zip(hue_colors, hue_values):
+        df_ = boutdf_filt[boutdf_filt[hue]==cond]
+        vals = df_[plotvar].values
+        bins = np.linspace(vmin, vmax, nbins) #vals.min(), vals.max(), 200)
+        ax2.hist(vals, bins, facecolor=col, edgecolor='none',  alpha=0.9)
+    ax2.set_xlabel(xlabel)
+    ax2.set_ylabel('number of bouts')
+
+    for ax in fig.axes:
+        ax.tick_params(which='both', axis='both', length=2, width=0.5, color='w',
+                       direction='out', left=True, bottom=True)
+        for pos in ['right', 'top']:
+           ax.spines[pos].set_visible(False)
+        #ax.set_box_aspect(1)
+
+    sns.despine(offset=4)
+
+    return fig
+
+
+
+def plot_sorted_distn_with_hist_instrip(varn, boutdf_filt, estimator='median',
+                             plot_bars=False, errorbar=('ci', 95),
                                 instrip_palette={True: 'r', False: 'w'}):
+    '''
+    Plot each individual fly's bouts as dot plot, abutting inside and outside.
+    Below dot plot, show abutting histograms aligned.
+
+    '''
     xlabels = {
         'crosswind_dist_range': 'crosswind range from edge (mm)',
         'duration':'duration',
@@ -231,7 +378,7 @@ def plot_one_flys_trials(df_, instrip_palette={True: 'r', False: 'w'},
 
 
 def plot_array_of_trajectories(trajdf, sorted_eff=[], nr=5, nc=7, aspect_ratio=0.5,
-                            bool_colors=['r'], bool_vars=['instrip']):
+                            bool_colors=['r'], bool_vars=['instrip'], title='filename'):
 
     if len(sorted_eff)==0:
         sorted_eff = trajdf['filename'].unique()
@@ -259,7 +406,11 @@ def plot_array_of_trajectories(trajdf, sorted_eff=[], nr=5, nc=7, aspect_ratio=0
         ax.set_ylim([-100, 1600])
         ax.axis('off')
         ax.set_aspect(aspect_ratio)
-        ax.set_title('{}:\n{}'.format(fi, fn), fontsize=6, loc='left')
+        if title=='filename':
+            ax_title = fn
+        else:
+            ax_title = df_['fly_id'].unique()[0]
+        ax.set_title('{}:\n{}'.format(fi, ax_title), fontsize=6, loc='left')
 
     for ax in axn.flat[fi:]:
         ax.axis('off')
